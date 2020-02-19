b06ca6203ddb7036389fd63e2f90fb07f6d8830b
istoking <58592678+istoking@users.noreply.github.com>
2020-02-19 20:00:30 +1300

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,8 +1,7 @@
 Config              = {}
 Config.DrawDistance = 100
-Config.Size         = {x = 1.5, y = 1.5, z = 1.5}
-Config.Color        = {r = 0, g = 128, b = 255}
 Config.Type         = 27
+Config.Type2        = 29
 
 Config.Locale = 'en'
 
@@ -10,8 +9,6 @@ Config.Zones = {
 
 	blackmarket = {
 		Items = {},
-		Pos = {
-            { x = -586.65,y = -1600.24,z = 27.01 }
-		}
+		Pos = vector3(-586.65, -1600.24, 27.01)
 	}
 }
