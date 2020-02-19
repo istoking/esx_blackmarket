c2cd22031f5e0d97a5e07eb0ea668feb9279c777
istoking <58592678+istoking@users.noreply.github.com>
2020-02-20 00:26:20 +1300

    Update config.lua
    My bad, I somehow forgot a setting and a }

--- config.lua
+++ config.lua
@@ -1,5 +1,7 @@
 Config              = {}
 Config.DrawDistance = 100
+Config.Size         = {x = 1.5, y = 1.5, z = 1.5}
+Config.Color        = {r = 139, g = 16, b = 20}
 Config.Type         = 27
 Config.Type2        = 29
 
@@ -9,6 +11,8 @@ Config.Zones = {
 
 	blackmarket = {
 		Items = {},
-		Pos = vector3(-586.65, -1600.24, 27.01)
+		Pos = {
+			vector3(-586.65, -1600.24, 27.01)
+		}
 	}
 }
